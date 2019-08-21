9f39ba03201e1575a279fbb2d61aaedcc335f1ac
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-21 20:35:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :roster 
+
+  def initialize (name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+    else 
+      @roster[grade] = [] # @roster.key?{grade}
+      @roster[grade] << student 
+    end 
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort 
+    @roster.each do |grade, name|
+      name.sort! # @roster.each_value( &:sort! )
+    end
+end
